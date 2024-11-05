9f8f4cad36a31de70f3bcf2e13c7809b5fc33bb3
Ingo <ingocodingbecauseyes@gmail.com>
2024-11-05 09:37:21 +0800

    added a signin in addition to signup

--- Code/app.py
+++ Code/app.py
@@ -15,8 +15,18 @@ def signup():
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
-        email = request.form['email']
+        email = request.form['email'] # these 3 are obvious
+        user_id = str(uuid.uuid4()) # unqiue user id
+        timestamp = datetime.now().strftime('%Y-%m-%d %H:%M:%S') # date of account creation
+
+        account_info = f"User ID: {user_id}\nUsername: {username}\nPassword: {password}\nCreated At: {timestamp}\n"
+        with open(f'accounts/{user_id}_account.txt', 'w') as file:
+            file.write(account_info)
     return render_template("signup.html")
+
+@app.route("/signin")
+def signin():
+    return render_template("signin.html")
 
 @app.route("/chat")
 def chat():
